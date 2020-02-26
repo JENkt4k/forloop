bfbd50335873800e7c6e9bb0c580f832dd00b4f9
James Nelson <james.e.nelson@gmail.com>
2020-02-26 17:30:16 -0600

    cool

--- script.js
+++ script.js
@@ -3,12 +3,31 @@ import {default as cars} from './cars.js'; //if not processed by webpack >2.0
 // reference: https://stackoverflow.com/questions/33650399/es6-modules-implementation-how-to-load-a-json-file?lq=1
 // https://stackoverflow.com/questions/33650399/es6-modules-implementation-how-to-load-a-json-file?lq=1
 
-function processJSON(data) {
+function processJsonNoTemplate(data) {
   const carDiv = document.getElementById("car-collection");
   const carArray = data["cars"];
   for (let i = 0; i < carArray.length; i++) {
     carDiv.appendChild(renderCar(carArray[i]));
   }
+}
+
+function processJSON(data){
+  const carDiv = document.getElementById("car-collection");
+  carDiv.innerHTML = data["cars"].map(car => templateRender(car)).join("");
+}
+
+function templateRender(carJson){
+  return `
+  <div class="car-div">
+          <img class="car-img" src ="${carJson["src"]}">
+          <div class="car-details">
+            <div class="car-model">${carJson["model"]}</div>
+            <div class="car-make detail">Manufacturer: ${carJson["make"]}</div> 
+            <div class="car-color detail">Exterior Color: ${carJson["color"]}</div>             
+          </div>
+        </div>
+  `;
+
 }
 
 function renderCar(carJson) {
